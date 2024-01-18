51de74a108c2a7373b6a0c55cd5c10a8797e5cd7
LUIGIBLASQUEZ <luigiblasquez@gmail.com>
2024-01-17 18:23:53 -0800

    constant documentation change

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,70 +1,64 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
+// Imports
 import com.revrobotics.CANSparkMax.IdleMode;
-
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
 
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide
- * numerical or boolean
- * constants. This class should not be used for any other purpose. All constants
- * should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
- *
- * <p>
- * It is advised to statically import this class (or one of its inner classes)
- * wherever the
- * constants are needed, to reduce verbosity.
+/* Constants class holds static variables that are referrenced in other classes
+ * This class should not have any functions or other purposes
+ * 
  */
 public final class Constants {
+
+  // Drive constants pertain to the handling and motors of the drivetrain
   public static final class DriveConstants {
-    // Driving Parameters - Note that these are not the maximum capable speeds of
-    // the robot, rather the allowed maximum speeds
+    
+    // Driving parameters
     public static final double kMaxSpeedMetersPerSecond = 1.0; //set to 4.0
     public static final double kMaxAngularSpeed = 2 * Math.PI; // radians per second
 
+    // Slew rates
     public static final double kDirectionSlewRate = 1.2; // radians per second
     public static final double kMagnitudeSlewRate = 1.8; // percent per second (1 = 100%)
     public static final double kRotationalSlewRate = 2.0; // percent per second (1 = 100%)
 
-    // Chassis configuration
+    // Swerve drivetrain physical distances
     public static final double kTrackWidth = Units.inchesToMeters(21.5);
-    // Distance between centers of right and left wheels on robot
     public static final double kWheelBase = Units.inchesToMeters(21.5);
-    // Distance between front and back wheels on robot
+
+    // Kinematics variables
     public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(
         new Translation2d(kWheelBase / 2, kTrackWidth / 2),
         new Translation2d(kWheelBase / 2, -kTrackWidth / 2),
         new Translation2d(-kWheelBase / 2, kTrackWidth / 2),
         new Translation2d(-kWheelBase / 2, -kTrackWidth / 2));
 
-    // Angular offsets of the modules relative to the chassis in radians
+    // Angular offset variables (radians)
     public static final double kFrontLeftChassisAngularOffset = -Math.PI / 2;
     public static final double kFrontRightChassisAngularOffset = 0;
     public static final double kBackLeftChassisAngularOffset = Math.PI;
     public static final double kBackRightChassisAngularOffset = Math.PI / 2;
 
-    // SPARK MAX CAN IDs
+    // SPARK MAX motor controller CAN Ids 
+    // Driving motors
     public static final int kFrontLeftDrivingCanId = 2;
     public static final int kRearLeftDrivingCanId = 6;
     public static final int kFrontRightDrivingCanId = 4;
     public static final int kRearRightDrivingCanId = 8;
-
+    // Turning motors
     public static final int kFrontLeftTurningCanId = 1;
     public static final int kRearLeftTurningCanId = 5;
     public static final int kFrontRightTurningCanId = 3;
     public static final int kRearRightTurningCanId = 7;
 
+    // Is gyro reversed?
     public static final boolean kGyroReversed = false;
   }
 
+  //
   public static final class ModuleConstants {
     // The MAXSwerve module can be configured with one of three pinion gears: 12T, 13T, or 14T.
     // This changes the drive speed of the module (a pinion gear with more teeth will result in a
