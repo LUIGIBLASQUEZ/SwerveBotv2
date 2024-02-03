9ef402b84599f5f830be9e0504a2c20338591542
Gautham <korrapatigautham@gmail.com>
2024-02-03 13:20:00 -0800

    stuff

--- src/main/java/frc/robot/commands/limelightControls/TargetTrackingCommand.java
+++ src/main/java/frc/robot/commands/limelightControls/TargetTrackingCommand.java
@@ -44,7 +44,7 @@ public class TargetTrackingCommand extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return m_lime.m_LimelightSteerCommand == 0 ;
+    return m_lime.m_LimelightSteerCommand == 0;
   }
 }
 
