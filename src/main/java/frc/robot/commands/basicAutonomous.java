875faa84070ebe1f62be3040a026f255925c291c
LUIGIBLASQUEZ <luigiblasquez@gmail.com>
2024-03-01 19:05:41 -0800

    supposed basic autonomous
    Co-authored-by: Failure-Tech <Failure-Tech@users.noreply.github.com>

---
+++ src/main/java/frc/robot/commands/basicAutonomous.java
@@ -0,0 +1,49 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants.DriveConstants;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import frc.robot.subsystems.DriveTrain;
+
+public class basicAutonomous extends CommandBase{
+    private DriveTrain m_robotDrive;
+
+    public void basicAuto(DriveTrain drive) {
+        m_robotDrive = drive;
+    }
+
+        public void initialize() {
+            m_robotDrive.zeroHeading();
+        }
+      
+        // When scheduled, run
+        public void execute() {
+          double xSpeed = 2.0;
+          double ySpeed = 2.0;
+          double rot = 1.0;
+
+          // Like drive command in RobotContainer
+          //xSpeed = -MathUtil.applyDeadband(0.5, 0.0);
+          //ySpeed = -MathUtil.applyDeadband(0.5, 0.0);
+          //rot = -MathUtil.applyDeadband(0.5, 0.0);
+    
+          // Make new ChassisSpeeds object to work with module states
+          ChassisSpeeds speeds;
+          speeds = new ChassisSpeeds(xSpeed, ySpeed, rot);
+    
+          SwerveModuleState[] moduleStates = DriveConstants.kDriveKinematics.toSwerveModuleStates(speeds);
+          m_robotDrive.setModuleStates(moduleStates);
+        }
+    
+        // If command ends or is interrupted, calls the method
+        public void end(boolean interrupted) {
+          m_robotDrive.setX();
+        }
+    
+        // Returns the end of the scheduled command
+        public boolean isFinished() {
+            return false;
+        }
+        
+    }
